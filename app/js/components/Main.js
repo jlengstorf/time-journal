eec0b5be5abefbd58a173ccfba64a00d2f9849b2
jlengstorf <jason.lengstorf@copterlabs.com>
2016-09-07 17:58:25 +0200

    feat(router): set up routing

--- app/js/components/Main.js
+++ app/js/components/Main.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { Router, Route, browserHistory } from 'react-router';
 import bemmit from '../util/bemmit';
 import firebase from '../helper/firebase';
+
+import UserInfo from './UserInfo';
+import Journal from './Journal';
+import Trends from './Trends';
+import Settings from './Settings';
 
 const handleLogOut = event => {
   event.preventDefault();
@@ -12,7 +18,12 @@ const Main = props => {
   const getClass = bemmit('app-shell');
   return (
     <div className={getClass()}>
-      <a href="#log-out" onClick={handleLogOut}>log out</a>
+      <UserInfo user={props.user} />
+      <Router history={browserHistory}>
+        <Route path="/"  component={Journal} />
+        <Route path="trends" component={Trends} />
+        <Route path="settings" component={Settings} />
+      </Router>
     </div>
   );
 };
